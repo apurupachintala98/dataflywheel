2a17293ca63036450dd8b681f8737ca7b74848f8
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-05-02 17:48:57 +0530

    push

--- src/pages/HomeContent/index.tsx
+++ src/pages/HomeContent/index.tsx
@@ -144,66 +144,19 @@ const HomeContent = () => {
             });
           }
         } else if (response.type === "sql") {
-          // setMessages(prev => [
-          //   ...prev,
-          //   { text: response.interpretation, fromUser: false },
-          //   {
-          //     text: response.sql,
-          //     fromUser: false,
-          //     isCode: true,
-          //     showExecute: true,
-          //     sqlQuery: response.sql,
-          //     type: "sql",
-          //     interpretation: response.interpretation,
-          //   },
-          // ]);
-          // setMessages(prev => {
-          //   const temp = [...prev];
-          //   temp[temp.length - 1] = {
-          //     ...temp[temp.length - 1],
-          //     fromUser: false,
-          //     isCode: true,
-          //     showExecute: true,
-          //     sqlQuery: response.sql,
-          //     type: "sql",
-          //     interpretation: response.interpretation,
-          //     text: response.sql,
-          //     streaming: false,
-          //   };
-          //   return temp;
-          // });
-          setMessages(prev => {
-            const temp = [...prev];
-            const last = temp[temp.length - 1];
-          
-            if (last?.streaming && !last.fromUser) {
-              temp[temp.length - 1] = {
-                ...last,
-                fromUser: false,
-                isCode: true,
-                showExecute: true,
-                sqlQuery: response.sql,
-                type: "sql",
-                interpretation: response.interpretation,
-                text: response.sql,
-                streaming: false,
-              };
-            } else {
-              temp.push({
-                fromUser: false,
-                isCode: true,
-                showExecute: true,
-                sqlQuery: response.sql,
-                type: "sql",
-                interpretation: response.interpretation,
-                text: response.sql,
-                streaming: false,
-              });
-            }
-          
-            return temp;
-          });
-          
+          setMessages(prev => [
+            ...prev,
+            { text: response.interpretation, fromUser: false },
+            {
+              text: response.sql,
+              fromUser: false,
+              isCode: true,
+              showExecute: true,
+              sqlQuery: response.sql,
+              type: "sql",
+              interpretation: response.interpretation,
+            },
+          ]);
         } 
       }      
     });
