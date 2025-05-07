3bbc4464d1aa781ba20da35fd291bd1a171397d0
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-05-07 18:02:57 +0530

    push

--- src/highcharts-setup.ts
+++ src/highcharts-setup.ts
@@ -1,17 +1,17 @@
 import Highcharts from 'highcharts';
 
 export const setupHighchartsModules = async () => {
-  const HighchartsMore = (await import('highcharts/highcharts-more')).default;
-  const VariablePie = (await import('highcharts/modules/variable-pie')).default;
-  const Exporting = (await import('highcharts/modules/exporting')).default;
-  const Accessibility = (await import('highcharts/modules/accessibility')).default;
-  const Highcharts3D = (await import('highcharts/highcharts-3d')).default;
+  const { default: HighchartsMore } = await import('highcharts/highcharts-more');
+  const { default: VariablePie } = await import('highcharts/modules/variable-pie');
+  const { default: Exporting } = await import('highcharts/modules/exporting');
+  const { default: Accessibility } = await import('highcharts/modules/accessibility');
+  const { default: Highcharts3D } = await import('highcharts/highcharts-3d');
 
-  HighchartsMore(Highcharts);
-  VariablePie(Highcharts);
-  Exporting(Highcharts);
-  Accessibility(Highcharts);
-  Highcharts3D(Highcharts);
+  if (typeof HighchartsMore === 'function') HighchartsMore(Highcharts);
+  if (typeof VariablePie === 'function') VariablePie(Highcharts);
+  if (typeof Exporting === 'function') Exporting(Highcharts);
+  if (typeof Accessibility === 'function') Accessibility(Highcharts);
+  if (typeof Highcharts3D === 'function') Highcharts3D(Highcharts);
 };
 
 export default Highcharts;
