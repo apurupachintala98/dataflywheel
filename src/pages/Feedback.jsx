337effebe125abc69c2f4c400b2c27214429cd19
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-05-08 17:50:50 +0530

    push

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -158,11 +158,17 @@ const MessageWithFeedback = ({ message, executeSQL, apiCortex, handleGraphClick 
                 {message.type === 'sql' && !sqlState.hidden ? (
                     <><Box sx={{ position: 'relative', mb: 2 }}>
                         {message.interpretation && (
-                        //      <Box
-                        //      sx={{ mt: 1, mb: 1, whiteSpace: 'pre-line' }}
-                        //      dangerouslySetInnerHTML={{ __html: message.interpretation }}
-                        //    />
-                        <Typography sx={{ mb: 1 }}>{message.interpretation}</Typography>
+                        <Box
+                        sx={{
+                          mt: 1,
+                          mb: 1,
+                          whiteSpace: 'pre-line',
+                          width: '100%',
+                        }}
+                        dangerouslySetInnerHTML={{ __html: message.interpretation }}
+                      />
+                      
+                        // <Typography sx={{ mb: 1 }}>{message.interpretation}</Typography>
                         )}
                         <Box sx={{
                             position: 'absolute',
